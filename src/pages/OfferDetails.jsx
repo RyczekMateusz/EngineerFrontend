4497604f3fcb1cf840652f4e9a1962542c428614
RyczekMateusz <ryczek.mateusz@gmail.com>
2022-12-04 20:40:58 +0100

    init photo carousel

--- src/pages/OfferDetails.jsx
+++ src/pages/OfferDetails.jsx
@@ -1,14 +1,28 @@
 import { useParams } from 'react-router-dom'
 import { useGetOfferById } from '../api/offers/hooks'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel'
 
 const OfferDetails = () => {
   let { offerId } = useParams()
 
   const { data: offer } = useGetOfferById({ offerId })
 
+  if (!offer) {
+    return null
+  }
+
   return (
     <div className="offer-details-wrapper">
       <h1>{offer?.offerName}</h1>
+      <Carousel width="700px">
+        {offer.photos.map((photo, index) => (
+          <div key={index}>
+            <img src={photo} />
+          </div>
+        ))}
+      </Carousel>
+
       <ul>
         <li>{offer?.address.city}</li>
         <li>{offer?.address.street}</li>
