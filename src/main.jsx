d3f6714d6d47f36368f04abaf4df2f8ee50bebc1
RyczekMateusz <ryczek.mateusz@gmail.com>
2023-01-10 10:15:01 +0100

    debug production url

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import UserProvider from './context/UserContext'
 import './i18n'
+import axios from 'axios'
+
+const devURL = 'http://localhost:8000'
+const prodURL = 'https://engineerbackend-production.up.railway.app/'
+
+const baseURL = import.meta.env.DEV ? devURL : prodURL
+
+axios.defaults.baseURL = baseURL
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
