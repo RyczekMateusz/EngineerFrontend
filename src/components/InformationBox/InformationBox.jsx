0533c0caa7fd73d7a3d455ee65af60a300c93711
RyczekMateusz <ryczek.mateusz@gmail.com>
2023-04-25 19:23:26 +0200

    offers details styles

--- src/components/InformationBox/InformationBox.jsx
+++ src/components/InformationBox/InformationBox.jsx
@@ -11,43 +11,39 @@ const InformationBox = ({ owner, offer }) => {
   const { t } = useTranslation()
 
   return (
-    <div className="offer-details__info_box">
-      <div>
-        <h4>{t('CONTACT_DETAILS')}</h4>
-        <p>
-          <UserIcon className="offer-details__icon" />
-          {owner.firstName}
-        </p>
-        <p>
-          <EmailIcon className="offer-details__icon" />
-          {owner.email}
-        </p>
-        <p>
-          <PhoneIcon className="offer-details__icon" />
-          {owner.phoneNumber}
-        </p>
-      </div>
+    <div className="info-box">
+      <h4 className="info-box__heading">{t('CONTACT_DETAILS')}</h4>
+      <p className="info-box__detail">
+        <UserIcon className="info-box__icon" />
+        {owner.firstName}
+      </p>
+      <p className="info-box__detail">
+        <EmailIcon className="info-box__icon" />
+        {owner.email}
+      </p>
+      <p className="info-box__detail">
+        <PhoneIcon className="info-box__icon" />
+        {owner.phoneNumber}
+      </p>
 
-      <div>
-        <h4>{t('OFFER_DETAILS')}</h4>
-        <p className="offer-details__location-wrapper">
-          <LocationIcon className="offer-details__icon" />
-          {offer?.address.city}, {offer?.address.district}, {offer?.address.street}
-        </p>
+      <h4 className="info-box__heading">{t('OFFER_DETAILS')}</h4>
+      <p className="info-box__location-detail">
+        <LocationIcon className="info-box__icon" />
+        {offer?.address.city}, {offer?.address.district}, {offer?.address.street}
+      </p>
 
-        <p>
-          <PriceIcon className="offer-details__icon" />
-          {t('RENT_ZL_PER_MONTH', { price: offer.price })}
-        </p>
-        <p>
-          <AreaIcon className="offer-details__icon" />
-          {t('LIVING_AREA_NUMBER', { area: offer.area })}
-        </p>
-        <p>
-          <BedIcon className="offer-details__icon" />
-          {t('NUMBER_OF_ROOMS_NUMBER', { number: offer?.roomsNumber })}
-        </p>
-      </div>
+      <p className="info-box__detail">
+        <PriceIcon className="info-box__icon" />
+        {t('RENT_ZL_PER_MONTH', { price: offer.price })}
+      </p>
+      <p className="info-box__detail">
+        <AreaIcon className="info-box__icon" />
+        {t('LIVING_AREA_NUMBER', { area: offer.area })}
+      </p>
+      <p className="info-box__detail">
+        <BedIcon className="info-box__icon" />
+        {t('NUMBER_OF_ROOMS_NUMBER', { number: offer?.roomsNumber })}
+      </p>
     </div>
   )
 }
