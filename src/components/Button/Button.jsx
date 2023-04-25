dfc1e3c052306d23acfa6968718ccfb9239aecb1
RyczekMateusz <ryczek.mateusz@gmail.com>
2023-04-25 18:53:48 +0200

    offers listing styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,16 @@
 import clsx from 'clsx'
 import { NavLink } from 'react-router-dom'
 
-const Button = ({ linkTo = null, label, onClick = () => {}, customClass, state = {}, key = 1 }) => {
+const Button = ({ linkTo = null, label, onClick = () => {}, customClass, state = {} }) => {
   if (!linkTo) {
     return (
-      <button key={key} className={clsx('defaultButton', customClass)} onClick={onClick}>
+      <button className={clsx('defaultButton', customClass)} onClick={onClick}>
         {label}
       </button>
     )
   }
   return (
-    <NavLink key={key} state={state} className={clsx('defaultButton', customClass)} onClick={onClick} to={linkTo}>
+    <NavLink state={state} className={clsx('defaultButton', customClass)} onClick={onClick} to={linkTo}>
       {label}
     </NavLink>
   )
